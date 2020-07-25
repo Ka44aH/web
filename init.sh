4dffd179838d8414b094ab756d74f08c6c89a857
Ka44aH <64129960+Ka44aH@users.noreply.github.com>
2020-07-25 20:12:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -6,3 +6,21 @@ sudo /etc/init.d/nginx restart
 
 Запуск gunicorn
 sudo gunicorn -b 0.0.0.0:8080 hello:app
+
+Для обновления до версии Django2.1 использую следующие команды:
+
+sudo apt-get update
+
+sudo apt-get install -y python3.5
+
+sudo apt-get install -y python3.5-dev
+
+sudo unlink /usr/bin/python3
+
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+
+sudo pip3 install --upgrade pip
+
+sudo pip3 install --upgrade django==2.1
+
+sudo pip3 install --upgrade gunicorn
