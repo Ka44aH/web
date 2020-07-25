daffe20b65dd907fb8bff0cabd55e026bfa3d2d2
Ka44aH <64129960+Ka44aH@users.noreply.github.com>
2020-07-25 21:08:36 +0300

    Update urls.py

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path                                                    
-from . import views                                                             
+from django.urls import path                                                                                                              
 
                                                                                 
 
