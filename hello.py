e95f66dd0d3eb98c3fc56c4398fa26a4cc3501cc
Ka44aH <64129960+Ka44aH@users.noreply.github.com>
2020-07-25 17:59:37 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ def wsgi_application(environ, start_response):
                                                                       
     body = [ bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&'
 )]                                                                              
-    start_response('200 OK', [('Content-Type', 'text/html')])                                            
+    start_response('200 OK', [('Content-Type', 'text/plain')])                                            
     return body
